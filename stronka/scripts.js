7e97355c45f23a6a153e5580ea5e8c65e4dcb48c
mynamescheff <92156558+mynamescheff@users.noreply.github.com>
2024-02-08 19:17:36 +0100

    added

---
+++ stronka/scripts.js
@@ -0,0 +1,8 @@
+function openOverlay(overlayId) {
+    document.getElementById(overlayId).style.width = "100%";
+  }
+  
+  function closeOverlay(overlayId) {
+    document.getElementById(overlayId).style.width = "0%";
+  }
+  
