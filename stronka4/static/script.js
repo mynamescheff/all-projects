fbcf5d8ef68bbbaa162e42284c9031d0e6743eec
mynamescheff <92156558+mynamescheff@users.noreply.github.com>
2024-02-17 21:05:27 +0100

    updated

--- stronka4/static/script.js
+++ stronka4/static/script.js
@@ -8,3 +8,19 @@ $ = function(id) {
   var hide = function(id) {
       $(id).style.display ='none';
   }
+
+  window.onload = function() {
+    var popups = document.getElementsByClassName('popup');
+    for (var i = 0; i < popups.length; i++) {
+        var close = popups[i].getElementsByClassName("close")[0];
+        close.onclick = function() {
+            this.parentElement.parentElement.style.display = "none";
+        }
+    }
+
+    window.onclick = function(event) {
+        if (event.target.className === "popup") {
+            event.target.style.display = "none";
+        }
+    }
+};
