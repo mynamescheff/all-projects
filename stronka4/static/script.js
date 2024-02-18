1aa86069f68d691cbd58b11f68ea6e3951af5edc
mynamescheff <92156558+mynamescheff@users.noreply.github.com>
2024-02-18 21:28:04 +0100

    updated

--- stronka4/static/script.js
+++ stronka4/static/script.js
@@ -1,26 +1,32 @@
-$ = function(id) {
-    return document.getElementById(id);
-  }
-  
-  var show = function(id) {
-      $(id).style.display ='block';
-  }
-  var hide = function(id) {
-      $(id).style.display ='none';
-  }
-
-  window.onload = function() {
-    var popups = document.getElementsByClassName('popup');
-    for (var i = 0; i < popups.length; i++) {
-        var close = popups[i].getElementsByClassName("close")[0];
-        close.onclick = function() {
-            this.parentElement.parentElement.style.display = "none";
+document.addEventListener('DOMContentLoaded', () => {
+    // Get all buttons that opens modals
+    let modalBtns = document.querySelectorAll('[id$="Btn"]'); // Selects all buttons ending with 'Btn'
+    
+    modalBtns.forEach(function(btn) {
+        btn.onclick = function() {
+            let modal = document.getElementById(btn.id.replace('Btn', 'Modal'));
+            if(modal) {
+                modal.style.display = "block";
+            }
         }
-    }
+    });
 
+    // Get all elements that closes modals
+    let closeButtons = document.querySelectorAll('.close');
+    
+    closeButtons.forEach(function(btn) {
+        btn.onclick = function() {
+            let modal = btn.closest('.modal');
+            if(modal) {
+                modal.style.display = "none";
+            }
+        }
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-        if (event.target.className === "popup") {
+        if (event.target.classList.contains('modal')) {
             event.target.style.display = "none";
         }
     }
-};
+});
