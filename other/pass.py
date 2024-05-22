28126bed9fe2ccea4cdbb1a3b5528fd33aca9112
mynamescheff <92156558+mynamescheff@users.noreply.github.com>
2024-05-22 19:26:48 +0200

    updated

--- other/pass.py
+++ other/pass.py
@@ -1,7 +1,9 @@
 import random
 import string
+import sys
+import os
 
-def generate_random_password(length=12):
+def generate_random_password(length=int(random.randint(18, 25))):
     # Define character sets
     uppercase_letters = string.ascii_uppercase
     lowercase_letters = string.ascii_lowercase
@@ -14,13 +16,27 @@ def generate_random_password(length=12):
     # Shuffle the characters in a custom way
     random.shuffle(all_characters)
     shuffled_characters = all_characters[:length]
-
+    # Make sure to include at least one character from each set
+    shuffled_characters.extend([random.choice(uppercase_letters), random.choice(lowercase_letters), random.choice(digits), random.choice(special_characters)])
     # Create the final password by joining the shuffled characters
     password = ''.join(shuffled_characters)
     
     return password
 
+# Function to save the history of generated passwords to a file in the same directory as current script
+def save_password_to_file(password):
+    # Get the directory of the script
+    script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    # Change the directory to the script directory
+    os.chdir(script_dir)
+    # Save the password to the file
+    with open("passwords.txt", "a") as file:
+        file.write(password + "\n")
+
 if __name__ == "__main__":
     # Generate a random password with a default length of 12 characters
     password = generate_random_password()
     print("Random Password:", password)
+    # Save the generated password to a file
+    save_password_to_file(password)
+    print("Password saved to file.")
