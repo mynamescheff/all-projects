e0c081a6801ab2f41d0ebae033835c054190034f
mynamescheff <92156558+mynamescheff@users.noreply.github.com>
2024-06-08 21:53:33 +0200

    updated

--- other/converter_excel_to_csv.py
+++ other/converter_excel_to_csv.py
@@ -16,3 +16,8 @@ sheet = wb['Sheet1']
 #convert the sheet to csv
 openpyxl.worksheet.csv.writer(sheet, os.path.join(path, 'data.csv'))
 
+#convert csv to .txt
+with open(os.path.join(path, 'data.csv'), 'r') as file:
+    data = file.read()
+    with open(os.path.join(path, 'data.txt'), 'w') as txt:
+        txt.write(data)
